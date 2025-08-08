0d9add53c84d18b4cf6676ea24186e9b3ab44ef2
Rishi Khemani <rishikhemani10@gmail.com>
2025-08-08 13:19:23 +0530

    created better UI

--- index.php
+++ index.php
@@ -52,6 +52,7 @@ if (isset($_SESSION['company_id'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css"
         integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="manifest" href="/manifest.json">
 </head>
 
 <body class="bg-gray-50 text-gray-800 font-sans ">
